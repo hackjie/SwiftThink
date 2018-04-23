254dda85960f9a75e3e35ca6a8219086e2920637
leoli <codelijie@gmail.com>
2018-04-23 17:56:07 +0800

    alamofire

---
+++ SwiftThink/SwiftThink/AlamofireLearn/AlamofireViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AlamofireViewController.swift
+//  SwiftThink
+//
+//  Created by leoli on 2018/4/23.
+//  Copyright © 2018年 leoli. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class AlamofireViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        testOne()
+    }
+
+    func testOne() {
+        Alamofire.request(TestAPI).responseJSON { (dataResponse) in
+            print(dataResponse)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
--- SwiftThink/SwiftThink/ViewController.swift
+++ SwiftThink/SwiftThink/ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: BaseViewController {
     }
 
     @objc func jump() {
-        let vc = DelegatedViewController()
+        let vc = AlamofireViewController()
         self.navigationController?.pushViewController(vc, animated: true)
     }
 }
