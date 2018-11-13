ef5cd6eb2fed48d6f3decdbf948ab7f57ee9a835
李杰 <lijie4@tuandai.com>
2018-11-13 11:52:32 +0800

    add playground

---
+++ Swift-集合类型优化/Swift-集合类型优化/SortedSet.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+public protocol SortedSet: BidirectionalCollection, CustomStringConvertible, CustomPlaygroundDisplayConvertible where Element: Comparable {
+    init()
+    func contains(_ element: Element) -> Bool
+    mutating func insert(_ newElement: Element) -> (inserted: Bool, memberAfterInsert: Element)
+}
+
+extension SortedSet {
+    public var description: String {
+        let contents = self.lazy.map { "\($0)" }.joined(separator: ", ")
+        return "[\(contents)]"
+    }
+}
